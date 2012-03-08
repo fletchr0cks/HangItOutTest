d9489d3fce767f29f96ebe020a013a5af9691c33
Nick Fletcher <nick.fletcher@gmail.com>
2012-03-08 14:06:33 +0000

    pics1

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,29 +5,31 @@ run(function () {
     // immediately invoked on first run
     var init = (function () {
         if (navigator.network.connection.type == Connection.NONE) {
-            alert("No internet connection - we won't be able to show you any maps");
+            //alert("No internet connection - we won't be able to show you any maps");
+            $('#data_result').html("No network connection. In demo mode");
         } else {
-            alert("We can reach Google! Trying location");
+            //alert("We can reach Google! Trying location");
             navigator.geolocation.getCurrentPosition(function (position) {
                 var loc = "" + position.coords.latitude + "," + position.coords.longitude;
-                $('#results').html(loc);
-                
+                                
                  $.ajax({
 		url: "http://api.wunderground.com/api/bf45926a1b878028/hourly/geolookup/q/56.058168,-2.719811.json",
 		dataType: "jsonp",
 		success: function(parsed_json) {
 			var location = parsed_json['location']['city'];
-            alert(location + loc);
+            //alert(location + loc);
+            $('#loc_result').html("Location is " + loc + ": " location);
+
            
              $.each(parsed_json.hourly_forecast, function (i, zone) {
             
             var sky = parseInt(zone.sky);
             
-            var userhtml = "<table style=\"width: 100%\"><tr><td style=\"width: 20%\"><div class=\"normal_small\">" + zone.FCTTIME.hour + "</div></td><td style\"width: 20%\"><div class=\"normal_small\">" + zone.temp.metric + "</td><td style=\"width: 20%\"><div class=\"normal_small\">" + zone.wspd.metric + "</td><td style=\"width: 20%\"><div class=\"normal_small\">" + zone.sky + "</div></td></tr></table>";
+            var userhtml = "<table style=\"width: 100%\"><tr><td style=\"width: 20%\"><div class=\"normal_small\">" + zone.FCTTIME.hour + "</div></td><td style\"width: 20%\"><div class=\"normal_small\">" + zone.temp.metric + "</td><td style=\"width: 20%\"><div class=\"normal_small\">" + zone.wspd.metric + "</td><td style=\"width: 20%\"><div class=\"normal_small\">" + zone.sky + "</div></td><td style=\"width: 20%\"><div class=\"normal_small\">" + zone.qpf.metric + "</div></td></tr></table>";
 
             
             $('#results2').append(userhtml);
-            $('#chart').html("<img src=\"http://chart.apis.google.com/chart?chxt=y&chs=300x150&cht=gm&chl=Groovy&chtt=Washing-o-meter&chco=FF9900&chd=t:70\" />");
+            $('#chart').html("<img src=\"http://chart.apis.google.com/chart?chxt=y&chs=300x150&cht=gm&chl=Do%20it&chtt=Washing-o-meter&chco=FF9900&chd=t:70\" />");
 
 	});
             }
@@ -36,14 +38,11 @@ run(function () {
                 
 
             }, function () {
-                $('#results2').html("fail");
+                $('#loc_result').html("Location not available. Using North Berwick.");
             });
             
             
-            
-            $('#results').html("Loading results ...");
-            
-   
+               
 
         }
     })();
